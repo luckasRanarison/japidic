6f7acc6fc0172f39b2dcd0c090871ae739f5b404
Luckas <luckasranarison@gmail.com>
2023-12-28 16:03:39 +0300

    wip: installation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,17 +3,26 @@ import "./index.css";
 import Navbar from "@/components/layout/navbar/Navbar";
 import Footer from "@/components/layout/footer/Footer";
 import { cookies } from "next/headers";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "japidic",
   description: "Free online japanese dictionary",
   keywords: ["japanese", "dictionary"],
+  manifest: "/manifest.json",
+  authors: [
+    {
+      name: "LIOKA Ranarison Fiderana",
+      url: "https://github.com/luckasRanarison",
+    },
+  ],
   openGraph: {
     title: "japidic",
     description: "Free online japanese dictionary",
     locale: "en_US",
     type: "website",
   },
+  icons: [{ rel: "icon", url: "/icons/icon-512x512.png" }],
 };
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
@@ -26,6 +35,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
       className={`${theme?.value === "dark" && "dark"}
       h-full overflow-y-auto`}
     >
+      <Script src="/register-sw.js" />
       <body
         className="flex flex-col h-full overflow-y-auto
         bg-light dark:bg-dark duration-300"
