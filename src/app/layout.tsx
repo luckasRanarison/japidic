9126db2cb24d611b8655d2d6c60d9bdcd23bdbfe
Luckas <luckasranarison@gmail.com>
2023-12-28 17:31:10 +0300

    feat: PWA (#10)
    * wip: installation
    
    * update: icons
    
    * update: manifest.json

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,19 @@ import "./index.css";
 import Navbar from "@/components/layout/navbar/Navbar";
 import Footer from "@/components/layout/footer/Footer";
 import { cookies } from "next/headers";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "japidic",
   description: "Free online japanese dictionary",
   keywords: ["japanese", "dictionary"],
+  manifest: "/manifest.json",
+  authors: [
+    {
+      name: "LIOKA Ranarison Fiderana",
+      url: "https://github.com/luckasRanarison",
+    },
+  ],
   openGraph: {
     title: "japidic",
     description: "Free online japanese dictionary",
@@ -26,6 +34,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
       className={`${theme?.value === "dark" && "dark"}
       h-full overflow-y-auto`}
     >
+      <Script src="/register-sw.js" />
       <body
         className="flex flex-col h-full overflow-y-auto
         bg-light dark:bg-dark duration-300"
